c466b8c8dea779e8726677e046e195f007247e5f
Gopinath6521 <49483589+Gopinath6521@users.noreply.github.com>
2025-04-18 16:14:11 +0530

    updated

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -12,8 +12,7 @@ terraform {
 provider "aws" {
   profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "ap-south-1"
-}asds
-asa
+}
 # Resource Block
 resource "aws_instance" "ec2demo" {
   ami           = "ami-002f6e91abff6eb96" # Amazon Linux in us-east-1, update as per your region
