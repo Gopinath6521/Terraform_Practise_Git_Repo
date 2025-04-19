cb4405830e20ada3d63f8de816026881199b904b
Gopinath6521 <49483589+Gopinath6521@users.noreply.github.com>
2025-04-19 15:41:59 +0530

    updated

--- Basics_Practise/c1-versions.tf
+++ Basics_Practise/c1-versions.tf
@@ -1,3 +1,11 @@
 terraform {
-  required_version = "~> 1.11.3"
+  #required_version = "~> 1.11.3"   # ~ allows versions till 1.11.4, 1.11.5 but deny 1.12.0 and above
+  required_version = "~> 1.11.3"    # ~ allows versions till 1.11.xx, 1.12.xx but deny 2.00 and above
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+
 }
