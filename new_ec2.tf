9d1d2ea219e60a7db1a647a5dc490946327168d7
Gopinath6521 <49483589+Gopinath6521@users.noreply.github.com>
2025-04-19 17:28:51 +0530

    updated

---
+++ new_ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "terraform-ec2" {
+    # arguments for ec2 
+  ami           = "ami-002f6e91abff6eb96" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+  key_name      = "terraform" # Update with your keypair name
+  user_data = file(user_data.sh) # Update with your script name
+  tags = {
+    Name = "terraform-ec2-instance"
+  }
+  # attributes are something whihc can be extractef from the code and used later
+}
