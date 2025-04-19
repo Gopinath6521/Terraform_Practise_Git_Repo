548d22603da91d04dd25c6cce9d3ecab31473b2b
Gopinath6521 <49483589+Gopinath6521@users.noreply.github.com>
2025-04-19 17:35:18 +0530

    updated

--- new_ec2.tf
+++ new_ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "terraform-ec2" {
   ami           = "ami-002f6e91abff6eb96" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
   key_name      = "terraform" # Update with your keypair name
-  user_data = file(user_data.sh) # Update with your script name
+  user_data = file("${path.module}/user_data.sh") # Update with your script name
   tags = {
     Name = "terraform-ec2-instance"
   }
